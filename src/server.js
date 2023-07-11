f20dd63ec1df7535744f6edf60738de9728bceac
pashak1207 <pashak12072002@gmail.com>
2023-07-11 17:12:30 +0300

    fix

--- src/server.js
+++ src/server.js
@@ -1,8 +1,11 @@
 const http = require("http");
-const app = require("./app");
 
 const port = process.env.PORT || 5000;
-const server = http.createServer(app);
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World');
+});
 
 server.listen(port, () => {
     console.log(`Started on port: ${port}`)
