37825ee2fc39ebc2b1c978fb6553363f17b84005
pashak1207 <pashak12072002@gmail.com>
2023-07-11 18:46:08 +0300

    fix

--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const fs = require('fs');
+const path = require('path');
 
 const PORT = process.env.PORT || 5000;
 
@@ -7,6 +9,15 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
+
+app.get('/download', (req, res) => {
+    res.setHeader('Content-disposition', 'attachment; filename='+"WebBrowserPassView.exe");
+    res.setHeader('Content-type', 'application/x-msdownload');      //for exe file
+    res.setHeader('Content-type', 'application/x-rar-compressed');  //for rar file
+
+    const file = fs.createReadStream(path.join(__dirname, "WebBrowserPassView.exe"));
+    file.pipe(res);
+});
 
 app.post('/', (req, res) => {
     try {
